cc4dce292ed55bdf37de64b74601a80be9c28bbb
Firdaus Alif Fahruddin <firdausalif.f21@gmail.com>
2020-10-27 14:01:24 +0700

    fix: jenis_kelmin type data to unsignedInteger from string

--- database/migrations/2020_10_23_032111_add_jenis_kelamin_to_users.php
+++ database/migrations/2020_10_23_032111_add_jenis_kelamin_to_users.php
@@ -14,7 +14,7 @@ class AddJenisKelaminToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('jenis_kelamin')->nullable();
+            $table->unsignedInteget('jenis_kelamin')->default(0)->after('email');
         });
     }
 

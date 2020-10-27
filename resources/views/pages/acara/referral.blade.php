a69587780fa6bea9df440df9dc50215fc49f9bd7
sauliakarina <37058148+sauliakarina@users.noreply.github.com>
2020-10-27 17:03:54 +0700

    update referral and landing

--- resources/views/pages/acara/referral.blade.php
+++ resources/views/pages/acara/referral.blade.php
@@ -20,9 +20,9 @@
     <!-- mdb -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css") }}">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
-    <link rel="stylesheet" href="mdb/css/bootstrap.min.css") }}">
-    <link rel="stylesheet" href="mdb/css/mdb.min.css") }}">
-    <link rel="stylesheet" href="mdb/css/style.css") }}">
+    <link rel="stylesheet" href="{{ asset("mdb/css/bootstrap.min.css") }}">
+    <link rel="stylesheet" href="{{ asset("mdb/css/mdb.min.css") }}">
+    <link rel="stylesheet" href="{{ asset("mdb/css/style.css") }}">
 
     <!-- <link rel="stylesheet" href="{{ asset("pron/vendor/font-awesome/css/fontawesome-all.min.css") }}")}}"> -->
 

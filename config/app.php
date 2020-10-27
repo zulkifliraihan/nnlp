d14052135bb88ff9a2e5c367a7f906b448e1a1d6
Firdaus Alif Fahruddin <firdausalif.f21@gmail.com>
2020-10-27 14:05:02 +0700

    add: yajra datatable for serverside admin user data

--- config/app.php
+++ config/app.php
@@ -175,7 +175,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Spatie\Permission\PermissionServiceProvider::class,
-        Maatwebsite\Excel\ExcelServiceProvider::class
+        Maatwebsite\Excel\ExcelServiceProvider::class,
+        Yajra\DataTables\DataTablesServiceProvider::class,
 
     ],
 
